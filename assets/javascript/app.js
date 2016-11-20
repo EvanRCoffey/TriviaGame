d44a5b582fced04b86c1b68d0d3a5b8b6d940170
molander86 <rogerecoffey@gmail.com>
2016-11-19 20:39:41 -0600

    Significant progress.  Javascript is nearly done.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,125 +2,169 @@ var numCorrect = 0;
 var numIncorrect = 0;
 var numTimeOut = 0;
 var currentQuestion = {};
+var timer = 30;
+var counterInterval;
+var counterTimeout;
 
 var Question1 = {
-	question = 'Who was the radio celebrity that provided comic relief throughout "The Fifth Element"?',
-	answer1 = "Jay Queue",
-	answer2 = "Ruby Rhod",
-	answer3 = "Dirk Diggler",
-	answer4 = "Chet Brockwell",
-	correctAnswer = 2;
+	question : "Who was the radio celebrity that provided comic relief throughout 'The Fifth Element'?",
+	answer1 : "Jay Queue",
+	answer2 : "Ruby Rhod",
+	answer3 : "Dirk Diggler",
+	answer4 : "Chet Brockwell",
+	correctAnswer : 2,
+	gif : "assets/images/rubyRhod.gif"
 }
 
 var Question2 = {
-	question = 'What was the name of the artificially intelligent computer featured in "2001: A Space Odyssey"?',
-	answer1 = "Quantum Express",
-	answer2 = "Watson",
-	answer3 = "HAL 9000",
-	answer4 = "THX 1138",
-	correctAnswer = 3;
+	question : 'What was the name of the artificially intelligent computer featured in "2001: A Space Odyssey"?',
+	answer1 : "Quantum Express",
+	answer2 : "Watson",
+	answer3 : "HAL 9000",
+	answer4 : "THX 1138",
+	correctAnswer : 3,
+	gif : "assets/images/2001.gif"
 }
 
 var Question3 = {
-	question = 'What city was the setting for "Blade Runner"?',
-	answer1 = "Tokyo",
-	answer2 = "Amsterdam",
-	answer3 = "Los Angeles",
-	answer4 = "Nashville",
-	correctAnswer = 3;
+	question : 'What city was the setting for "Blade Runner"?',
+	answer1 : "Tokyo",
+	answer2 : "Amsterdam",
+	answer3 : "Los Angeles",
+	answer4 : "Nashville",
+	correctAnswer : 3,
+	gif : "assets/images/bladeRunner.gif"
 }
 
 var Question4 = {
-	question = 'Who directed "Alien"?',
-	answer1 = "Stanley Kubrick",
-	answer2 = "Christopher Nolan",
-	answer3 = "The Wachowski Siblings",
-	answer4 = "Ridley Scott",
-	correctAnswer = 4;
+	question : 'Who directed "Alien"?',
+	answer1 : "Stanley Kubrick",
+	answer2 : "Christopher Nolan",
+	answer3 : "The Wachowski Siblings",
+	answer4 : "Ridley Scott",
+	correctAnswer : 4,
+	gif : "assets/images/alien.gif"
 }
 
 var Question5 = {
-	question = 'What was the tattoo that got Keanu Reeves out of his apartment in the opening scene of "The Matrix"?',
-	answer1 = "A white rabbit",
-	answer2 = "A butterfly",
-	answer3 = "A serpent coiled around a sword",
-	answer4 = "A fire-breathing dragon",
-	correctAnswer = 1;
+	question : 'What was the tattoo that got Keanu Reeves out of his apartment in the opening scene of "The Matrix"?',
+	answer1 : "A white rabbit",
+	answer2 : "A butterfly",
+	answer3 : "A serpent coiled around a sword",
+	answer4 : "A fire-breathing dragon",
+	correctAnswer : 1,
+	gif : "assets/images/matrix.gif"
 }
 
 var Question6 = {
-	question = 'What is "Wall-E"s favorite movie?',
-	answer1 = "2001: A Space Odyssey",
-	answer2 = "Hello, Dolly!",
-	answer3 = "Snow White and the Seven Dwarves",
-	answer4 = "West Side Story",
-	correctAnswer = 2;
+	question : 'What is "Wall-E"s favorite movie?',
+	answer1 : "2001: A Space Odyssey",
+	answer2 : "Hello, Dolly!",
+	answer3 : "Snow White and the Seven Dwarves",
+	answer4 : "West Side Story",
+	correctAnswer : 2,
+	gif : "assets/images/walle.gif"
 }
 
 var Question7 = {
-	question = 'What caused the accident that stranded Sandra Bullock in "Gravity"?',
-	answer1 = "A meteor shower",
-	answer2 = "An explosion in her ship",
-	answer3 = "Debris from a destroyed satellite",
-	answer4 = "Her harness breaking",
-	correctAnswer = 3;
+	question : 'What caused the accident that stranded Sandra Bullock in "Gravity"?',
+	answer1 : "A meteor shower",
+	answer2 : "An explosion in her ship",
+	answer3 : "Debris from a destroyed satellite",
+	answer4 : "Her harness breaking",
+	correctAnswer : 3,
+	gif : "assets/images/gravity.gif"
 }
 
 var Question8 = {
-	question = 'What was Matthew Mcconaugheys occupation before he agreed to help save the world in "Interstellar"?',
-	answer1 = "Farmer",
-	answer2 = "Pilot",
-	answer3 = "Web developer",
-	answer4 = "Preschool teacher",
-	correctAnswer = 2;
+	question : 'What was Matthew Mcconaugheys occupation before he agreed to help save the world in "Interstellar"?',
+	answer1 : "Farmer",
+	answer2 : "Pilot",
+	answer3 : "Web developer",
+	answer4 : "Preschool teacher",
+	correctAnswer : 2,
+	gif : "assets/images/interstellar.gif"
 }
 
 var questions = [Question1, Question2, Question3, Question4, Question5, Question6, Question7, Question8]
 
-//Do the following instructions 8 times:
-for (var i = 0; i < 8; i++) {
-	//Load the question object into current question
-	currentQuestion = questions[i];
-	//Set timer to 30 seconds and display time remaining in time remaining area
-	//???
-	//Display next question in message area
-	$("#messageArea").html(currentQuestion.question)
-	//Display that question's answers in answers area
-	$("#answersArea").html(currentQuestion.answer1)
-	$("#answersArea").append(currentQuestion.answer2)
-	$("#answersArea").append(currentQuestion.answer3)
-	$("#answersArea").append(currentQuestion.answer4)
-	//If user doesn't select an answer in 30 seconds
-	//???
-		//Display time out message in message area
-		$("#messageArea").html("Time's up!");
-		//Increment time outs
-		numTimeOut++;
-		//Stop timer
-		//???
-		//Display gif in answers area with a caption (and do anything else that's fun)
-		//???
-		//Wait 5 seconds
-		//???
-	//When user selects an answer
-	//???
-		//If the answer was correct
-		//???
-			//Display winning message in message area
-			$("#messageArea").html("Correct!");
-			//Increment correct answers
-			numCorrect++;
-		//Otherwise
-		else {
-			//Display losing message in message area
-			$("#messageArea").html("Incorrect!");
-			//Increment incorrect answers
-			numIncorrect++;
-		}
-		//Stop timer
-		//???
-		//Display gif in answers area with a caption (and do anything else that's fun)
-		//???
-		//Wait 5 seconds
-		//???
+function playFromBeginning () {
+	nextQuestion()
 }
+
+function nextQuestion() {
+	if ((numCorrect + numIncorrect + numTimeOut) === 8) {
+		//End-game status report
+		$("#messageArea").html("Total correct: " + numCorrect + "<br>");
+		$("#messageArea").append("Total incorrect: " + numIncorrect + "<br>");
+		$("#messageArea").append("Total time outs: " + numTimeOut + "<br>");
+		$("#messageArea").append("Thanks for playing!");
+	}
+	else {
+		//Set timer to 30 seconds
+		timer = 30;
+		//Load the question object into current question
+		currentQuestion = questions[(numCorrect+numIncorrect+numTimeOut)];
+		//Display time remaining in time remaining area
+		$("#timeRemaining").html("Time remaining: " + timer + " seconds.");
+		//Display next question in message area
+		$("#messageArea").html(currentQuestion.question);
+		//Display that question's answers in answers area
+		$("#answersArea").html('<button onclick="isCorrect(1)" type="button" class="btn btn-default"> id="1"' + currentQuestion.answer1 + '</button>');
+		$("#answersArea").append('<button onclick="isCorrect(2)" type="button" class="btn btn-default"> id="2"' + currentQuestion.answer2 + '</button>');
+		$("#answersArea").append('<button onclick="isCorrect(3)" type="button" class="btn btn-default"> id="3"' + currentQuestion.answer3 + '</button>');
+		$("#answersArea").append('<button onclick="isCorrect(4)" type="button" class="btn btn-default"> id="4"' + currentQuestion.answer4 + '</button>');
+		//Give user 30 seconds to answer question
+		counterInterval = setInterval(decrementTimer, 1000)
+		counterTimeout = setTimeout(timesUp, 30000);
+	}
+	
+}
+
+function decrementTimer() {
+	timer--;
+	$("#timeRemaining").html("Time remaining: " + timer + " seconds.");
+}
+
+function isCorrect(number) {
+	//If the answer is correct....
+	if (number === currentQuestion.correctAnswer) {
+		//Display winning message in message area
+		$("#messageArea").html("Correct!");
+		//Increment correct answers
+		numCorrect++;
+	}
+	//If the answer is incorrect...
+	else {
+		//Display losing message in message area
+		$("#messageArea").html("Incorrect!");
+		//Increment incorrect answers
+		numIncorrect++;
+	}
+	//Clear interval
+	clearInterval(counterInterval);
+	//Clear timer
+	clearTimeout(counterTimeout)
+	//Display gif in answers area with a caption (and do anything else that's fun)
+	$("#answersArea").html('<img src= "'+ currentQuestion.gif + '">')
+	//Wait 5 seconds, then move on to the next question
+	setTimeout(nextQuestion, 5000);
+}
+
+function timesUp() {
+	//Display time out message in message area
+	$("#messageArea").html("Time's up!");
+	//Increment time outs
+	numTimeOut++;
+	//Clear interval
+	clearInterval(counterInterval);
+	//Clear timer
+	clearTimeout(counterTimeout)
+	//Display gif in answers area with a caption (and do anything else that's fun)
+	$("#answersArea").html('<img src= "'+ currentQuestion.gif + '">')
+	//Wait 5 seconds, then move on to the next question
+	setTimeout(nextQuestion, 5000);
+}
+
+//Run the trivia game!
+playFromBeginning();
